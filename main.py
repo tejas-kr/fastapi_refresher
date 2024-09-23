8b9a4cc560057e4fa86809b8a5ee4838316f7765
Tejas K Jaiswal <tejasjaiswal9711@gmail.com>
2024-09-23 11:36:34 +0530

    removed hardcoded bands

--- main.py
+++ main.py
@@ -16,18 +16,6 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 
-BANDS = [
-    {'id': 1, 'name': 'AC/DC', 'genre': 'rock'},
-    {'id': 2, 'name': 'the Beatles', 'genre': 'rock', 'albums': [
-        {'title': 'Please Please Me', 'release_date': '1963-03-22'}
-    ]},
-    {'id': 3, 'name': 'the Judds', 'genre': 'country'},
-    {'id': 4, 'name': 'Black Sabbath', 'genre': 'metal', 'albums': [
-        {'title': 'paranoid', 'release_date': '1970-09-18'}
-    ]},
-]
-
-
 @app.get("/bands")
 async def get_all_bands(
     genre: Optional[GenreURLChoices] = None,
